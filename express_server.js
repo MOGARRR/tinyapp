e4a06e8d29006448e9e7904aa46ae57b9e4bf78d
AnthonyA <alicea.9a@gmail.com>
2024-11-28 13:33:08 -0700

    feat: Add POST route to update database for new urls and GETto redirect short urls to their long urls

--- express_server.js
+++ express_server.js
@@ -3,47 +3,52 @@ const app = express();
 const PORT = 8081;
 
 app.set('view engine','ejs'); // Sets out default engine to ejs
-app.use(express.urlencoded({extended:true})); // translates post request body so we can use it
+app.use(express.urlencoded({extended:true})); // creates and fills req.body
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-app.get('/', (req,res) => {
-  res.send('Hello!');
-});
+// app.get('/', (req,res) => { // test route
+//   res.send('Hello!');
+// });
 
 app.listen(PORT, () => {
-  console.log(`Example App listening on port ${PORT}`);
+  console.log(`Example App listening on port ${PORT}`); // start server and which port
 });
 
-app.get('/urls.json', (req,res) => {
+app.get('/urls.json', (req,res) => { // returns database json object
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req,res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get('/hello', (req,res) => { html test route
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get('/urls', (req,res) => {
+app.get('/urls', (req,res) => { // render index page
   const templateVars = {urls:urlDatabase};
   res.render('urls_index',templateVars);
 });
 
-app.post('/urls', (req,res) => {
-  console.log(req.body);
-  res.send('ok');
+app.post('/urls', (req,res) => { // post request to add new urls to database
+  const id = generateRandomString(); // make random url id
+  const url = req.body.longURL; // get url from parsed data from post
+  urlDatabase[id] = url;
+  res.redirect(`/urls/${id}`);
 });
 
-app.get('/urls/new', (req,res) => {
+app.get('/urls/new', (req,res) => { // renders create new url form
   res.render('urls_new');
 });
 
-
+app.get('/u/:id', (req,res) => {// redirects to the ids longURL in the database
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
 app.get('/urls/:id', (req,res) => { // catch all get requst for urls. add url pages above
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show',templateVars);
 });
 
-const generateRandomString = () => Math.random.toString(36).slice(2);
+const generateRandomString = () => Math.random().toString(36).slice(6);
