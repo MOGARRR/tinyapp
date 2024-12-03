b116859ab10ff37a96c65343d70ed33fe0e52d82
AnthonyA <alicea.9a@gmail.com>
2024-12-02 21:18:11 -0700

    refactor/feat: Add POST route to take form values and create cookie and new user to database. Change header partial to display user email when registered

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = 8081;
+const PORT = 8080;
 
 app.use(cookieParser()); // helps read values from cookies
 app.use(morgan('dev')); // console logs requests and status codes from server
@@ -13,6 +13,7 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+const users = {}
 
 
 app.listen(PORT, () => {
@@ -24,7 +25,7 @@ app.get('/urls.json', (req,res) => { // returns database json object
 });
 
 app.get('/urls', (req,res) => { // render index page
-  const templateVars = {urls:urlDatabase, username: req.cookies['username']};
+  const templateVars = {urls:urlDatabase, users , userCookie: req.cookies['user_id']};
   res.render('urls_index',templateVars);
 });
 
@@ -36,7 +37,7 @@ app.post('/urls', (req,res) => { // post request to add new urls to database
 });
 
 app.get('/urls/new', (req,res) => { // renders create new url form
-  const templateVars = {username: req.cookies['username']};
+  const templateVars = {users, userCookie: req.cookies['user_id']};
   res.render('urls_new', templateVars);
 });
 
@@ -59,7 +60,7 @@ app.post('/urls/:id/edit', (req,res) => { // changes short url values to new lon
 });
 
 app.get('/urls/:id', (req,res) => { // catch all get requst for urls. add url pages above
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies['username']};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], users, userCookie: req.cookies['user_id']};
   res.render('urls_show', templateVars);
 });
 
@@ -68,8 +69,26 @@ app.post('/login', (req,res) => { // Saves username POST request as a cookie and
   res.redirect('/urls');
 });
 
-app.post('/logout', (req,res) => {
+app.post('/logout', (req,res) => { // clears username cookie and redirects to url page
   res.clearCookie('username');
   res.redirect('/urls');
 });
+
+app.get('/register', (req,res) => { // renders register page templateVars for header partial
+  const templateVars = {users, userCookie: req.cookies['user_id']};
+  res.render('register', templateVars);
+});
+
+app.post('/register', (req,res) => { // on POST request 
+  const newID = generateRandomString(); // random str for new id
+  res.cookie('user_id', newID); // create cookie with id value
+  const newUser = { // new object with request form values and cookie/id value
+    id: newID,
+    email: req.body.email,
+    password: req.body.password
+  };
+  users[newID] = newUser; // adds new user object to global database
+  res.redirect('urls'); // redirects to home url page
+});
+
 const generateRandomString = () => Math.random().toString(36).slice(6);
